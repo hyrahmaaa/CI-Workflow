0d1bd114f943d420e941619852aad1d3583794b2
hyrahmaaa <chhoiharim@gmail.com>
2025-06-16 12:13:26 +0700

    Update modelling_tuning.py

--- MLProject/modelling_tuning.py
+++ MLProject/modelling_tuning.py
@@ -11,6 +11,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression 
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
+from inference import ChurnPredictor 
 import os
 
 # Import kelas ChurnPredictor dari inference.py
